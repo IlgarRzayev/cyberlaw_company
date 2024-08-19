6ecf759f399afe4b1e155c536f7195c7dae14f94
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-08-19 15:06:35 +0300

    Update config.php

--- source_code/config.php
+++ source_code/config.php
@@ -7,7 +7,7 @@ $database = "cyberlaw";
 $connection = mysqli_connect($host, $username, $password, $database,3307);
 
 if (mysqli_connect_errno() > 0) {
-    die("Bağlantı hatası: " . mysqli_connect_error()); 
+    die("Connection error: " . mysqli_connect_error()); 
 }
 
 //echo "Connection successful"; 
