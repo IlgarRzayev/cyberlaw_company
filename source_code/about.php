6ea0437b32fcf1b60d171c0df285c5b2dc56eae4
IlgarRzayev <92079654+IlgarRzayev@users.noreply.github.com>
2024-08-19 14:34:17 +0300

    Create about.php

---
+++ source_code/about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Haqqımızda</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+    
+    .about-body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, sans-serif;
+        background-color: #0b0e4a;
+        color: white;
+    }
+
+
+    .about-section {
+        padding: 20px;
+    }
+
+    .about-section h1 {
+        font-size: 36px;
+        margin-bottom: 20px;
+        margin-top: 30px;
+    }
+
+    .about-section p {
+        font-size: 16px;
+        line-height: 1.5;
+        margin-top: 40px;
+        margin-bottom: 100px;
+    }
+
+    .img-fluid {
+        border-radius: 8px;
+    }
+    .about-title{
+        margin: 20px;
+    }
+
+    </style>
+</head>
+<body class="about-body">
+<?php include 'navbar.php'; ?>
+
+    <div class="container mt-5">
+        <section class="about-section text-center">
+            <div>
+                <h1 class="about-title">Haqqımızda</h1>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur. Felis purus neque varius sit. Cras rhoncus rhoncus risus et hac eget vulputate vulputate id. In dolor id ornare proin senectus etiam. Semper nibh elit nunc at. Ut sagittis nibh ipsum ipsum. Amet egestas quam adipiscing lorem.</p>
+            </div>
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-6 mb-4 mb-md-0">
+                    <img src="images/img1.jpg" class="img-fluid" alt="Hacker image 1">
+                </div>
+                <div class="col-md-6">
+                    <img src="images/img2.jpg" class="img-fluid" alt="Hacker image 2">
+                </div>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur. Eget turpis eu volutpat maecenas velit. Ac amet quis ut neque nec nisl bibendum. Vulpate diam sed purus aliquam purus commodo sed. Diam sit arcu ut id leo. Laoreet velit cursus eget fames ut volutpat vitae pretium consectetur. Sed laoreet sem gravida fames etiam ut donec. Quis vitae facilisis odio morbi. Sit adipiscing in non condimentum fermentum viverra ac. Sit consequat sem tellus sit orci nibh risus maecenas. Eget dolor condimentum sit elit egestas. Ultricies faucibus molestie et mattis fringilla nunc venenatis cras. Vel et habitant pellentesque mattis eu. Iaculis volutpat dapibus sagittis aliquet malesuada blandit. Sit tristique volutpat sit amet arcu fusce sagittis euismod sem. Vel a ultricies sollicitudin eget mauris aliquet faucibus sit. Dolor vulputate feugiat rhoncus vivamus natoque. Duis mauris dui dictum egestas pellentesque sed cursus. Non scelerisque nec sagittis morbi id ut natoque. Nulla feugiat tellus ornare tincidunt leo morbi ut. Egestas a.
+                Viverra nequeibis dignissim orci elementum. Mauris at vitae non ut. Nibh quam quam ac amet ornare interdum nec. Imperdiet donec fames morla tortor aliquam id enim duis augue consectetur quis. Justo at commodo egestas adipiscing tempor fermentum. Vulpate at diam congue non.</p>
+            </div>
+        </section>
+    </div>
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
+
+    
